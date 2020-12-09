88e774fe2b0d481821980cd769aa33172d3a6b66
Kazutaka Homma <kazutaka@mercari.com>
2020-12-09 11:21:32 +0900

    add pods

--- Podfile
+++ Podfile
@@ -5,9 +5,9 @@ use_frameworks!
 target 'CurrencyConverter' do
     pod 'Alamofire', '~> 5.2'
     pod 'Swinject', '2.7.1'
-end
 
-target 'CurrencyConverterTests' do
-    pod 'Quick', '3.0.0'
-    pod 'Nimble', '9.0.0'
+    target 'CurrencyConverterTests' do
+        pod 'Quick', '3.0.0'
+        pod 'Nimble', '9.0.0'
+    end
 end
--- Pods/Target Support Files/Pods-CurrencyConverter-CurrencyConverterTests/Pods-CurrencyConverter-CurrencyConverterTests-umbrella.h
+++ Pods/Target Support Files/Pods-CurrencyConverter-CurrencyConverterTests/Pods-CurrencyConverter-CurrencyConverterTests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_CurrencyConverterTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CurrencyConverterTestsVersionString[];
+FOUNDATION_EXPORT double Pods_CurrencyConverter_CurrencyConverterTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CurrencyConverter_CurrencyConverterTestsVersionString[];
 
