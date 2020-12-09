af527948a800a8c053c34bffae2463a8d655e5ca
Kazutaka Homma <kazutaka@mercari.com>
2020-12-09 22:19:42 +0900

    add comment

--- CurrencyConverter/DataGateway.swift
+++ CurrencyConverter/DataGateway.swift
@@ -16,6 +16,7 @@ struct DataGateway: Gateway {
     let rateLimit: TimeInterval = 30 * 60
     
     let exchangeRateRequest: URLRequest = {
+        // free account does not let us use secure connection (https)
         let url = URL(string: "http://api.currencylayer.com/live?access_key=ac61db195ff8cc316e49808a6ab05245")!
         return URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 60)
     }()
