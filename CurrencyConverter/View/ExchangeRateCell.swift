d6f0093b24fdc7a2e63eae640b01032d459ec86e
Kaz <kazutaka@mercari.com>
2020-12-14 00:24:06 +0900

    add ExchangeRateCell

---
+++ CurrencyConverter/View/ExchangeRateCell.swift
@@ -0,0 +1,13 @@
+//
+//  ExchangeRateCell.swift
+//  CurrencyConverter
+//
+//  Created by kazutaka on 2020/12/12.
+//
+
+import UIKit
+
+class ExchangeRateCell: UICollectionViewCell {
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+}
