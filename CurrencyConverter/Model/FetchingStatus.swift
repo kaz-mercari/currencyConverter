2a3c54af98f5f58407ddbb83d5b9ad658821419f
Kaz <kazutaka@mercari.com>
2020-12-14 00:24:22 +0900

    add Fetching Status

---
+++ CurrencyConverter/Model/FetchingStatus.swift
@@ -0,0 +1,14 @@
+//
+//  FetchingStatus.swift
+//  CurrencyConverter
+//
+//  Created by kazutaka on 2020/12/13.
+//
+
+import Foundation
+
+enum FetchingStatus {
+    case success
+    case failure
+    case inProgress
+}
